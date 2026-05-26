29ee6e83241eb69807787ec1dee45a40ea9fa7ef
mblees <mariusblees@gmail.com>
2026-05-26 20:43:53 +0200

    implemented example code from lib

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,45 @@
 #include <Arduino.h>
 #include <LIS3DH.h>
+#include <Wire.h>
 
 LIS3DH lis3dh;
 
-void setup() {
-  Serial.begin(115200);
+void setup()
+{
+  Serial.begin(9600);
   // Wait briefly for serial monitor to attach on some boards
-  delay(2000);
+  while (!Serial);
+  delay(200);
+  Serial.println("Serial connected!");
+  
+  Wire.begin(21, 22);
 
-  if (!lis3dh.begin()) {
+  if (!lis3dh.begin())
+  {
     Serial.println("LIS3DH not found. Check wiring.");
-    while (1) delay(1000);
+    while (1)
+      delay(1000);
   }
 }
 
-void loop() {
+void loop()
+{
   float x, y, z;
   lis3dh.readAcceleration(x, y, z);
 
   float rel = lis3dh.getRelativeAcceleration();
-  Serial.print("X: "); Serial.print(x);
-  Serial.print(" Y: "); Serial.print(y);
-  Serial.print(" Z: "); Serial.print(z);
-  Serial.print(" | Relative: "); Serial.print(rel);
+  Serial.print("X: ");
+  Serial.print(x);
+  Serial.print(" Y: ");
+  Serial.print(y);
+  Serial.print(" Z: ");
+  Serial.print(z);
+  Serial.print(" | Relative: ");
+  Serial.print(rel);
   Serial.println(" m/s^2");
 
-  if (lis3dh.isMoving(2.0)) {
+  if (lis3dh.isMoving(2.0))
+  {
     Serial.println("Device is moving (threshold 2.0 m/s^2)");
   }
 
